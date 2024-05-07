c9d40072af12bdd10083740917ae6be976c5386b
long0167472 <Hoanglong7374@gmail.com>
2024-05-07 07:44:44 +0700

    add some thing

--- Backend/Backend/obj/Debug/net6.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net6.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa2df5e436f1a846f4f0a808e32584119d73bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
